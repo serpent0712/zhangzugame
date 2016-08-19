eac029fe23a9438e1747a169be7b9b4040267654
大盼 <jackzpp@jackzppdeMacBook-Pro.local>
2016-08-19 16:02:09 +0800

    Initial commit

---
+++ app/src/main/java/com/zhangzu/game/MainActivity.java
@@ -0,0 +1,19 @@
+package com.zhangzu.game;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zhangzu.game.utils.MD5Util;
+
+import app.zhangzu.com.zhangzugame.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MD5Util.MD5("jackzpp");
+
+    }
+}
