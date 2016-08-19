736ad29d2e763648eb6dd06600a1c26bb6f452f7
jackzpp <serpent0712>
2016-08-19 16:09:02 +0800

    问问

--- app/src/main/java/com/zhangzu/game/MainActivity.java
+++ app/src/main/java/com/zhangzu/game/MainActivity.java
@@ -12,8 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        MD5Util.MD5("jackzpp");
-
+        MD5Util.MD5("woani");
     }
 }
